a837daa783d32ed0eb4e8345c34dafdc125b0399
Aymen Chebbi <aymen.chebi@gmail.com>
2018-04-16 19:07:24 +0100

    Removed redux devtool

--- src/index.js
+++ src/index.js
@@ -2,19 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import thunk from 'redux-thunk'
 
-import { createStore, compose, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reducers from './reducers'
 
 import App from './components/App'
 
-const store = createStore(
-  reducers,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-)
+const store = createStore(reducers, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>

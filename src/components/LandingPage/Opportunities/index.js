195ef659b9e8a4782ae865f79795db16000eff61
AS <anis.kh86@gmail.com>
2018-04-16 20:22:40 +0100

    modif comment participer css landing page

--- src/components/LandingPage/Opportunities/index.js
+++ src/components/LandingPage/Opportunities/index.js
@@ -6,19 +6,19 @@ class Opportunities extends React.Component {
     return (
       <div className='opportunities-container'>
         <div className='container'>
-          <h2>Quels sont les métiers cibles de nos étudiants ?</h2>
+          <h2>les métiers cibles de nos étudiants ?</h2>
           <div className='row'>
-            <div className='col-sm-4'>
+            <div className='opportunity col-xs-10 col-sm-4'>
               <h3>Développeur dans une entreprise technologique</h3>
               <p className='mt10'>Développeur web dans une entreprise technologique comme un éditeur de logiciel, une SS2I, un site de e-commerce ou une marketplace.</p>
             </div>
 
-            <div className='col-sm-4'>
+            <div className='opportunity col-xs-10 col-sm-4'>
               <h3>Freelancer</h3>
               <p className='mt10'>Développeur web Freelancer: le freelancing est entrain de devenir une option de carrière. Plusieurs sites postent chaque jours des milliers de projets à réaliser en mode Freelance.</p>
             </div>
 
-            <div className='col-sm-4'>
+            <div className='opportunity col-xs-10 col-sm-4'>
               <h3>CTO ou co-fondateur</h3>
               <p className='mt10'>CTO ou co-fondateur d'une startup: notre programme est très orienté produit et permet aux étudiants de créer leur produit technologique au cours de la formation.</p>
             </div>
